5ae64bb35271f320be0a9b2144e14a1a97b32265
EdoSalas <esalas@betasolutions.tech>
2022-10-13 17:10:56 -0600

    Main | Apidoc Init

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,6 +8,30 @@ import http from "../model/enums/http";
 const router = Router();
 const userCtrl = new UserController();
 
+/**
+ * @api {get} /api/users/
+ * @apiVersion 1.0.0
+ * @apiGroup User
+ * @apiDescription Obtain all users
+ * @apiExample {curl} Example usage:
+ *      curl -i http://localhost:4041/api/users/
+ * @apiSuccess {Number} id Id of the User.
+ * @apiSuccess {String} name Name of the User.
+ * @apiSuccess {Date} dof Date of birth of the User.
+ * @apiSuccess {String} email Email of the User.
+ * @apiSuccess {Number} status Status of the User.
+ * @apiSuccessExample {json} Success-Response:
+ *      HTTP/1.1 200 OK
+ *      [
+ *          {
+ *              "id": 1,
+ *              "name": Eduardo Salas,
+ *              "dof": 12/12/1995,
+ *              "email": esalas95@outlook.com
+ *              "status": 1
+ *          }
+ *      ]
+ */
 router.get('/', async (req, res) => {
     try {
         return res.status(http.OK).json(new BaseResponse(
